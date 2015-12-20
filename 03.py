315a616eae111b8247a8e331e1f2a4a3caf9d1a0
john murray <john.murray@bluefinsolutions.com>
2015-12-20 21:10:54 +0000

    day 3 part 2

--- 03.py
+++ 03.py
@@ -21,8 +21,23 @@ def uniqueHouses(fileName):
     return len(set(map(tuple, houses)))
 
 
+def twoSantas(fileName):
+    directionsList = open(fileName)
+    santa = [[0,0]]
+    robot = [[0,0]]
+    for line in directionsList:
+        santaDirections = line[::2]
+        robotDirections = line[1::2]
+        for direction in santaDirections:
+            santa.append(newLoc(santa[-1], direction))
+        for direction in robotDirections:
+            robot.append(newLoc(robot[-1], direction))
+    return len(set(map(tuple, santa+robot)))
+
 
 if __name__ == '__main__':
+    # part 1
     print(uniqueHouses('03.txt'))
-
+    # part 2
+    print(twoSantas('03.txt'))
 
